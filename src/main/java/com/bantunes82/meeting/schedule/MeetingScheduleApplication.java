f3e5b98402e3c68f65d2e4d379ecb4e4aa33d4bc
Bruno Romao <bantunes82@gmail.com>
2026-02-07 10:46:20 +0100

    Spring boot skeleton

---
+++ src/main/java/com/bantunes82/meeting/schedule/MeetingScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.bantunes82.meeting.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bantunes82/meeting/schedule/MeetingScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bantunes82.meeting.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
